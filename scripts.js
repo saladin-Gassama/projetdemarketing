d1a5efad717b4cada63d27702272565cf2e01042
Salah Eddine Gassama <gassamasalaheddine@gmail.com>
2025-03-30 18:46:43 +0200

    projet de marketing

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+const btn1 = document.getElementById(*btn1*);
+const btn2 = document.getElementById(*btn2*);
+const btn3 = document.getElementById(*btn3*);
+const btn4 = document.getElementById(*btn4*);
+const btn5 = document.getElementById(*btn5*);
+const btn6 = document.getElementById(*btn6*);
+
+
+btn1.oneclick = () => document.body.style.backigroundColor = "";
+btn2.oneclick = () => document.body.style.backigroundColor "";
+btn3.oneclick = () => document.body.style.backigroundColor = "";
+btn4.oneclick = () => document.body.style.backigroundColor = "";
+btn5.oneclick = () => document.body.style.backigroundColor = "";
+btn6.oneclick = () => document.body.style.backigroundColor = "";
